a9c243c6bdb298de9ca5ddbdc65feb24adbda6bf
Nick Amin <amin.nj@hurr.tamu.edu>
2013-08-20 17:33:53 -0500

    Added parser for nicktest.py and added functionality to get number of cores

--- usermonparser.py
+++ usermonparser.py
@@ -17,19 +17,33 @@ for line in inp.readlines():
     username = v[0]
     runningJobs = int(v[1])
     queuedJobs = int(v[2])
+    nCores = 0
+    if(len(v)>4):
+        nCores = int(v[3])
+        #print nCores
     if(utcTime not in data):
         data[utcTime] = []
     
-    data[utcTime].append([username, runningJobs, queuedJobs])
+    data[utcTime].append([username, runningJobs, queuedJobs, nCores])
+
+def getParameter(e, parameter):
+    if(parameter is None): return e[1]+e[2]
+    if(parameter == "running"): return e[1]
+    if(parameter == "queued"): return e[2]
+    if(parameter == "total jobs"): return e[1]+e[2]
+    if(parameter == "cores"): return e[3]
+    if(parameter == "cores per job"): 
+        if e[1] == 0: return 0
+        else: return e[3]/e[1]
     
-def getJobUsage(data, filterByUser=None):
+def getJobUsage(data, filterByUser=None, parameter=None):
     timestamps = []
     values = []
 
     for key in data:
         timestamps.append(key)
         
-        values.append(sum([e[1]+e[2] for e in data[key] if (e[0]==filterByUser or filterByUser is None)]))
+        values.append(sum([getParameter(e,parameter) for e in data[key] if (e[0]==filterByUser or filterByUser is None)]))
     
     temp = zip(timestamps, values)
     temp.sort()
@@ -55,7 +69,7 @@ ax.xaxis.set_major_formatter(xfmt)
 ax.set_yscale('log')
 
 for user in users:
-    dates, values = getJobUsage(data, user)
+    dates, values = getJobUsage(data, user, "total jobs")
     plt.plot(dates,values,label=user)
 ax.legend(bbox_to_anchor=(0.9, 1.0), loc=2, borderaxespad=0.)
 plt.show()
